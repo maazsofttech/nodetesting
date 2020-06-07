adc2378351c1c2c05e2669c65771f6b3ba21f048
maaz antastic in <maaz@antastic.in>
2020-06-07 21:42:24 +0530

    First api connected to mongo

--- server.js
+++ server.js
@@ -9,8 +9,54 @@ app.get('/listUsers', function (req, res) {
    });
 })
 
-var server =  app.listen(3456, "127.0.0.1", function () {
-   var host = server.address().address
-   var port = server.address().port
-   console.log("Example app listening at http://%s:%s", host, port)
-})
+
+const MongoClient = require('mongodb').MongoClient;
+
+var url = "mongodb+srv://freemen:freemen%40mongomongo@freemen-eo4l4.mongodb.net/maaz?retryWrites=true&w=majority";
+
+const client = new MongoClient(url, { useUnifiedTopology: true });  
+
+const dbName = "maaz";
+
+
+
+ app.get('/users', function (req, res) {
+    
+
+client
+      .connect()
+      .then(
+        client =>
+          client
+            .db(dbName)
+            .collection("freemen").find()
+            .toArray()  
+      )
+    //   .then(cols => console.log("Collections", cols))
+.then(cols => res.end(  JSON.stringify(cols) ))
+      .finally(() => client.close());
+
+
+      
+   
+ })
+
+
+
+
+
+/*
+const uri = "mongodb+srv://freemen:freemen%40mongomongo@freemen-eo4l4.mongodb.net/maaz?retryWrites=true&w=majority";
+const client = new MongoClient(uri, { useNewUrlParser: true });
+client.connect(err => {
+  const collection = client.db("test").collection("freemen");
+console.log(collection.find())
+  // perform actions on the collection object
+  client.close();
+});
+*/
+const port = process.env.PORT || 3000
+
+app.listen(port,() => {
+  console.log("Server running at port "+port);
+});
