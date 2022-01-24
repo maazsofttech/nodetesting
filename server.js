baa8c1e63aef6614d67667ffa42d12c13a9c3902
Mohd Maaz <maazsofttech@gmail.com>
2022-01-24 16:20:23 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,14 +1,14 @@
 var express = require('express');
 var app = express();
 var fs = require("fs");
-
+/*
 app.get('/listUsers', function (req, res) {
    fs.readFile( "users.json", 'utf8', function (err, data) {
       console.log( data );
       res.end( data );
    });
 })
-
+*/
  
 
 const MongoClient = require('mongodb').MongoClient;
